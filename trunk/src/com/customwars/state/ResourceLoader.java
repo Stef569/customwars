552c538d039e150cd175d541a8352c94183f06ff
appletvdesign <appletvdesign@16bf2f10-a5f7-11dd-a887-7ffe1a420f8d>
2008-12-12 02:41:54 +0000

    Removed the need to hardcode project locations. Added resources to output build and made them reachable from the classloader. I have still maintained the disconnect between the retireving of resources and the main code as we want to move to a plugin architecture the locations of the sourced options will change as the user decides upon different addons. This can already be seen in affect in the test resources.

---
+++ trunk/src/com/customwars/state/ResourceLoader.java
@@ -0,0 +1,54 @@
+package com.customwars.state;
+
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+public class ResourceLoader {
+	
+	public static Properties properties;
+	final static Logger logger = LoggerFactory.getLogger(ResourceLoader.class);
+		
+	public static void init() {
+		properties = new Properties(); 
+		loadProperties();
+		FileSystemManager.init();
+	}
+	
+	public static void initTestProperties() {
+		properties = new Properties();
+		loadUnitTestProperties();
+		FileSystemManager.init();
+	}
+
+
+	public static void loadProperties(){
+		logger.info("Loading Live Game Properties");
+		properties.setProperty("optionsLocation", ResourceLoader.class.getResource("/core/options/options").getFile().toString());
+		properties.setProperty("BaseDMGLocation", ResourceLoader.class.getResource("/core/damagetables/BaseDMG.txt").getFile().toString());
+		properties.setProperty("AltDMGLocation",  ResourceLoader.class.getResource("/core/damagetables/AltDMG.txt").getFile().toString());
+		properties.setProperty("imagesLocation", ResourceLoader.class.getResource("/core/images").getFile().toString());
+		properties.setProperty("soundLocation", ResourceLoader.class.getResource("/core/sound").getFile().toString());
+		properties.setProperty("tempSaveLocation", ResourceLoader.class.getResource("/save/temp.save").getFile().toString());
+		properties.setProperty("mapsLocation", ResourceLoader.class.getResource("/core/maps").getFile().toString());
+		properties.setProperty("saveLocation", ResourceLoader.class.getResource("/save").getFile().toString());
+	}
+	
+	private static void loadUnitTestProperties() {
+		logger.info("Loading Unit Test Properties");
+		properties.setProperty("optionsLocation", ResourceLoader.class.getResource("/testres/options/options").getFile().toString());
+		properties.setProperty("BaseDMGLocation", ResourceLoader.class.getResource("/testres/damagetables/BaseDMG.txt").getFile().toString());
+		properties.setProperty("AltDMGLocation", ResourceLoader.class.getResource("/testres/damagetables/AltDMG.txt").getFile().toString());
+		properties.setProperty("imagesLocation", ResourceLoader.class.getResource("/testres/images").getFile().toString());
+		properties.setProperty("soundLocation", ResourceLoader.class.getResource("/testres/sound").getFile().toString());
+		properties.setProperty("tempSaveLocation", ResourceLoader.class.getResource("/testres/save").getFile().toString());
+		properties.setProperty("mapsLocation", ResourceLoader.class.getResource("/testres/maps").getFile().toString());
+		properties.setProperty("saveLocation", ResourceLoader.class.getResource("/testres/save").getFile().toString());
+	}
+	
+    
+
+}
