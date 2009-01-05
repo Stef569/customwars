860f55eff3c60fa7b0ee94312cb128c131ddadd9
Ace.Stef <Ace.Stef@16bf2f10-a5f7-11dd-a887-7ffe1a420f8d>
2009-01-05 19:42:11 +0000

    Adding State Support to menus

--- trunk/v1/client/src/com/customwars/ui/menus/Mainmenu.java
+++ trunk/v1/client/src/com/customwars/ui/menus/Mainmenu.java
@@ -1,10 +1,8 @@
 package com.customwars.ui.menus;
 
-import com.customwars.ai.GameSession;
-import com.customwars.sfx.SFX;
-import com.customwars.state.ResourceLoader;
 import com.customwars.ui.MainMenuGraphics;
-import com.customwars.ui.menu.MenuSession;
+import com.customwars.ui.state.State;
+import com.customwars.ui.state.StateManager;
 
 import javax.swing.*;
 import java.awt.*;
@@ -19,9 +17,9 @@ import java.awt.event.MouseEvent;
  * @author stefan
  * @since 2.0
  */
-public class Mainmenu extends Menu {
+public class Mainmenu extends Menu implements State {
     private static final String COPYRIGHT_SYMBOL = "\u00a9";
-    private static final String COPYRING = "Advance Wars is " + COPYRIGHT_SYMBOL + " Nintendo/Intelligent Systems";
+    private static final String COPYRIGHT = "Advance Wars is " + COPYRIGHT_SYMBOL + " Nintendo/Intelligent Systems";
 
     private static final Font COPYRIGHT_FONT = new Font(Font.SANS_SERIF, Font.PLAIN, 14);
     private static final Color COPYRIGHT_COLOR = Color.WHITE;
@@ -32,28 +30,33 @@ public class Mainmenu extends Menu {
     private static final int NUM_MENU_ITEMS = 3;
 
     private JFrame frame;
-    private MenuSession menuSession;
+    private StateManager stateManager;
     private KeyControl keyControl = new KeyControl();
     private MouseControl mouseControl = new MouseControl();
 
-    public Mainmenu(JFrame frame, MenuSession menuSession) {
+    public Mainmenu(JFrame frame, StateManager stateManager) {
         super(NUM_MENU_ITEMS);
         this.frame = frame;
-        this.menuSession = menuSession;
-        initInput(frame);
+        this.stateManager = stateManager;
     }
 
-    public void initInput(JFrame frame) {
+    public void init() {
         frame.addKeyListener(keyControl);
         frame.addMouseListener(mouseControl);
     }
 
-    protected void paintMenu(Graphics2D g) {
+    public void stop() {
+        frame.removeKeyListener(keyControl);
+        frame.removeMouseListener(mouseControl);
+    }
+
+    // PAINT
+    public void paint(Graphics2D g) {
         g.drawImage(MainMenuGraphics.getTitleBackground(), 0, 0, frame);
         paintMenu(g, getCurrentMenuItem());
     }
 
-    public void paintMenu(Graphics2D g, int currentMenuItem) {
+    private void paintMenu(Graphics2D g, int currentMenuItem) {
         switch (currentMenuItem) {
             case NEW_GAME:
                 g.drawImage(MainMenuGraphics.getNewGame(true), 0, 0, frame);
@@ -78,9 +81,10 @@ public class Mainmenu extends Menu {
 
         g.setFont(COPYRIGHT_FONT);
         g.setColor(COPYRIGHT_COLOR);
-        g.drawString(COPYRING, 100, 310);
+        g.drawString(COPYRIGHT, 100, 310);
     }
 
+    // INPUT
     private class KeyControl extends KeyAdapter {
         public void keyPressed(KeyEvent e) {
             switch (e.getKeyCode()) {
@@ -143,34 +147,16 @@ public class Mainmenu extends Menu {
 
         switch (currentMenuItem) {
             case NEW_GAME:
-                menuSession.changeToState("NEW_GAME");
+                stateManager.changeToState("NEW_GAME");
                 break;
             case MAP_EDITOR:
-                menuSession.changeToState("MAP_EDITOR");
+                stateManager.changeToState("MAP_EDITOR");
                 break;
             case OPTIONS:
-                menuSession.changeToState("OPTIONS");
+                stateManager.changeToState("OPTIONS");
                 break;
             default:
                 throw new AssertionError("Could not handle current menu item: " + currentMenuItem);
         }
-    }
-
-    public static void main(String[] args) {
-        ResourceLoader.init();
-        String SOUND_LOCATION = ResourceLoader.properties.getProperty("soundLocation");
-        SFX.setSoundLocation(SOUND_LOCATION);
-        JFrame frame = new JFrame();
-        GameSession.mainFrame = frame;
-        MainMenuGraphics.loadImages(frame);
-        final Mainmenu mainMenu = new Mainmenu(frame, new MenuSession());
-        JPanel panel = new JPanel() {
-            protected void paintComponent(Graphics g) {
-                mainMenu.paintMenu((Graphics2D) g);
-            }
-        };
-        frame.add(panel);
-        frame.setSize(480, 320);
-        frame.setVisible(true);
     }
 }
