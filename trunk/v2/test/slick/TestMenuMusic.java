6a71cd3096d8e98e8c087da895785eae061ee59f
ctomni231 <ctomni231@16bf2f10-a5f7-11dd-a887-7ffe1a420f8d>
2009-02-06 20:21:25 +0000

    Ok, this revision adds a simple menu onto the slick class. It also tests to see if images work... which they do. All that we need now is music!

--- trunk/v2/test/slick/TestMenuMusic.java
+++ trunk/v2/test/slick/TestMenuMusic.java
@@ -5,6 +5,7 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.Sound;
 import org.newdawn.slick.Input;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
@@ -15,26 +16,41 @@ import org.newdawn.slick.state.StateBasedGame;
  */
 public class TestMenuMusic extends BasicGameState {
   private Sound menuTick;
+  private Image image;
+  private Image cursor;
+  private int option;
 
   public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
     menuTick = new Sound("v2/res/sound/menutick.wav");
+    image = new Image("v2/res/image/cliff.gif");
+    cursor = new Image("v2/res/image/white.png");
+    option = 0;
   }
 
   public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics g) throws SlickException {
+    g.drawImage(image, 0, 0);
     g.drawString("Now in Test Menu and Music state", 80, 80);
+    g.drawString("Option 1", 100, 100);
+    g.drawString("Option 2", 100, 120);
+    g.drawString("Option 3", 100, 140);
+    
+    //One line... all action?
+    g.drawImage(cursor, 80, (100+(option*20)));
   }
 
   public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException {
-
   }
 
   public void keyPressed(int key, char c) {
     if(key == Input.KEY_A) {
       menuTick.play();
     }
-  }
-
-  public int getID() {
-    return 0;
-  }
+    
+    if(key == Input.KEY_UP && option > 0){
+        option--;
+    }
+    
+    if(key == Input.KEY_DOWN && option < 2){
+        option++;
+    }
 }
