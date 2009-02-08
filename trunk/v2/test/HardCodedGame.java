1766a5c9e775a6b70b78b543edd87c193194aefa
Ace.Stef <Ace.Stef@16bf2f10-a5f7-11dd-a887-7ffe1a420f8d>
2009-02-08 01:48:57 +0000

    #35 moving hard coded game 1 lvl up, it can be used in slick tests, but also in model tests.

---
+++ trunk/v2/test/HardCodedGame.java
@@ -0,0 +1,45 @@
+package test;
+
+import client.model.map.Tile;
+import client.model.map.TileMap;
+import client.model.map.gameobject.Terrain;
+
+/**
+ * Hardcoded game data, useful for testing
+ */
+public class HardCodedGame {
+  public static final int MOVE_INF = 0;
+  public static final int MOVE_MECH = 1;
+  public static final int MOVE_TREAD = 2;
+  public static final int MOVE_TIRES = 3;
+  public static final int MOVE_AIR = 4;
+  public static final int MOVE_NAVAL = 5;
+
+  // Movecosts: INF, MECH, TREAD, TIRES, AIR, NAVAL
+  public static Byte[] plainMoveCosts = new Byte[]{1, 1, 1, 2, 1, Terrain.IMPASSIBLE};
+  public static Byte[] riverMoveCosts = new Byte[]{1, 1, Terrain.IMPASSIBLE, Terrain.IMPASSIBLE, 1, Terrain.IMPASSIBLE};
+
+  public static Terrain plain = new Terrain(0, "plain", "", (byte) 0, (byte) 0, plainMoveCosts);
+  public static Terrain verticalRiver = new Terrain(20, "River", "", (byte) 0, (byte) -1, riverMoveCosts);
+  private static TileMap<Tile> map = new TileMap<Tile>(10, 10, 32);
+
+  public static TileMap<Tile> getMap() {
+    fillWithTerrain(plain);
+    map.getTile(0, 3).setTerrain(verticalRiver);
+    map.getTile(0, 1).setTerrain(verticalRiver);
+    map.getTile(0, 2).setTerrain(verticalRiver);
+    map.getTile(5, 5).setFogged(true);
+    map.getTile(6, 5).setFogged(true);
+    map.getTile(4, 5).setFogged(true);
+    return map;
+  }
+
+  private static void fillWithTerrain(Terrain terrain) {
+    for (int col = 0; col < map.getCols(); col++) {
+      for (int row = 0; row < map.getRows(); row++) {
+        Tile t = new Tile(col, row, terrain);
+        map.setTile(col, row, t);
+      }
+    }
+  }
+}
