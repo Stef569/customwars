5a4616226f4270b0847ec4d019d8b5cdf67878ac
ctomni231 <ctomni231@16bf2f10-a5f7-11dd-a887-7ffe1a420f8d>
2009-02-10 20:35:45 +0000

    #35: Normalized the code and coding style according to the Intellij format. Can't deal with the mouse as Slick provides no way of dealing with the width of the font lettering. Until problem is dealt with, mouse actions will have to wait. Planning to add Sound functionality in the near future.

--- trunk/v2/test/slick/TestMenu.java
+++ trunk/v2/test/slick/TestMenu.java
@@ -1,119 +1,112 @@
-package slick;
+package test.slick;
 
-import com.customwars.client.ui.CWInput;
 import org.newdawn.slick.Color;
-import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
-import org.newdawn.slick.SlickException;
 import org.newdawn.slick.command.Command;
-import org.newdawn.slick.state.BasicGameState;
-import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.command.InputProviderListener;
+import client.ui.CWInput;
 
 /**
  * Shows a menu on the screen
  */
-public class TestMenu extends BasicGameState {
 
-  private String[] options;
-  private Color baseColor;
-  private Color selectColor;
-  private int locx;
-  private int locy;
-  private Image cursor;
-  private int curoptn;
-  private CWInput input;
-
-  TestMenu(int noOfOptions, CWInput input) {
-    this.input = input;
-    locx = 0;
-    locy = 0;
-    curoptn = 0;
-    cursor = null;
-    options = new String[noOfOptions];
-    baseColor = new Color(Color.white);
-    selectColor = new Color(Color.black);
-  }
-
-  public void changeLocation(int x, int y) {
-    locx = x;
-    locy = y;
-  }
-
-  public void changeImage(Image theImage) {
-    cursor = theImage;
-  }
-
-  public void changeColor(Color base, Color selected) {
-    baseColor = base;
-    selectColor = selected;
-  }
-
-  public void numberOfOptions(int noOfOptions) {
-    String[] temp = options;
-    options = new String[noOfOptions];
-    for (int i = 0; i < options.length; i++) {
-      options[i] = "";
-      if (i < temp.length)
-        options[i] = temp[i];
+public class TestMenu{
+    
+    private String[] options;
+    private Color baseColor;
+    private Color selectColor;
+    private int locx;
+    private int locy;
+    private Image cursor;
+    private int curoptn;
+      
+    TestMenu(int noOfOptions){
+      locx = 0;
+      locy = 0;
+      curoptn = 0;
+      cursor = null;
+      options = new String[noOfOptions];
+      baseColor = new Color(Color.white);
+      selectColor = new Color(Color.black);
     }
-  }
-
-  public void optionName(int index, String name) {
-    if (index >= 0 && index < options.length) {
-      if (name != null)
-        options[index] = name;
+    
+    public void setLocation(int x, int y){
+      locx = x;
+      locy = y;
     }
-  }
-
-  public void selectUp() {
-    if (curoptn > 0)
-      curoptn--;
-  }
-
-  public void selectDown() {
-    if (curoptn + 1 < options.length)
-      curoptn++;
-  }
-
-  public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
-  }
-
-  public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics g) throws SlickException {
-    for (int i = 0; i < options.length; i++) {
-      if (curoptn == i)
-        g.setColor(selectColor);
-      else
-        g.setColor(baseColor);
-
-      if (cursor == null)
-        g.drawString(options[i], locx, locy + (i * 20));
-      else
-        g.drawString(options[i], cursor.getWidth() + 5 + locx, locy + (i * 20));
+    
+    public void setImage(Image theImage){
+      cursor = theImage;
+    }
+    
+    public void setColor(Color base, Color selected){
+      baseColor = base;
+      selectColor = selected;
+    }
+    
+    public void setSize(int noOfOptions){
+      String[] temp = options;
+      options = new String[noOfOptions];
+      for(int i = 0; i < options.length; i++){
+        options[i] = "";
+        if(i < temp.length)
+           options[i] = temp[i];
+      }
+    }
+    
+    public void setOptionName(String name) {
+      for(int i = 0; i < options.length; i++){
+        if(name != null){
+           if(options[i] == null || options[i].matches("")){
+             options[i] = name;
+             return;
+           }
+        }
+      }
+    }
+    
+    public void setOptionName(int index, String name){
+      if(index >= 0 && index < options.length){
+        if(name != null)
+          options[index] = name;
+      }
+    }
+    
+    public void moveUp(){
+      if(curoptn > 0)
+        curoptn--;
+    }
+    
+    public void moveDown(){
+      if(curoptn+1 < options.length)
+        curoptn++;
     }
 
-    if (cursor != null)
-      g.drawImage(cursor, locx, (locy + (curoptn * 20)));
-
-    g.setColor(baseColor);
-  }
-
-  public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException {
-  }
-
-  public int getID() {
-    return 0;
-  }
-
-  public void controlPressed(Command command) {
-    if (input.isSelectPressed(command)) {
-      System.out.println("Pressed " + getID());
-    } else if (input.isNextMenuItemPressed(command)) {
-      selectDown();
-      //menuTickSound.play();
-    } else if (input.isPreviousMenuItemPressed(command)) {
-      selectUp();
-      //menuTickSound.play();
+    public void render(Graphics g){
+      for(int i = 0; i < options.length; i++){
+        if(curoptn == i)
+          g.setColor(selectColor);
+        else
+          g.setColor(baseColor);
+            
+        if(cursor == null)
+          g.drawString(options[i], locx, locy+(i*20));
+        else
+          g.drawString(options[i], cursor.getWidth()+5+locx, locy+(i*20));
+      }
+        
+      if(cursor != null)
+        g.drawImage(cursor, locx, (locy+(curoptn*20)));
+        
+      g.setColor(baseColor);
     }
-  }
+
+    public void controlPressed(Command command, CWInput cwInput) {
+      if (cwInput.isNextMenuItemPressed(command)) {
+        moveDown();
+      } else if (cwInput.isPreviousMenuItemPressed(command)) {
+        moveUp();
+      }
+    }
 }
--- trunk/v2/test/slick/TestMenuMusic.java
+++ trunk/v2/test/slick/TestMenuMusic.java
@@ -1,6 +1,6 @@
-package slick;
+package test.slick;
 
-import com.customwars.client.ui.CWInput;
+import client.ui.CWInput;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
@@ -9,65 +9,69 @@ import org.newdawn.slick.Music;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.Sound;
 import org.newdawn.slick.command.Command;
+import org.newdawn.slick.command.InputProviderListener;
+import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 /**
  * Shows a menu on the screen
  * when the menu is shown a sound is played
  */
-public class TestMenuMusic extends CWState {
+public class TestMenuMusic extends BasicGameState implements InputProviderListener {
   private Music backgroundMusic;
   private Sound menuTickSound;
   private Image image;
-  private CWInput input;
-  private TestMenu menu;
+  private Image cursor;
+  private int option;
+  private CWInput cwInput;
+  private TestMenu testmenu;
+  //BasicGameState game;
+  //game.mouseMoved(int oldx, int oldy, int newx, int newy);
 
   public TestMenuMusic(CWInput cwInput) {
-    this.input = cwInput;
+    this.cwInput = cwInput;
+    cwInput.addCommandListener(this);
+    testmenu = new TestMenu(3);
+    testmenu.setLocation(80,100);
   }
 
   public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
-    menuTickSound = new Sound("res/sound/menutick.wav");
-    backgroundMusic = new Music("res/sound/shortBackground.ogg");
+    menuTickSound = new Sound("v2/res/sound/menutick.wav");
+    backgroundMusic = new Music("v2/res/sound/shortBackground.ogg");
     backgroundMusic.setVolume(0.5F);
     backgroundMusic.loop();
 
-    image = new Image("res/image/cliff.gif");
-    buildTestMenu();
-  }
-
-  private void buildTestMenu() {
-    menu = new TestMenu(3, input);
-    menu.optionName(0, "Attack!");
-    menu.optionName(1, "Select");
-    menu.optionName(2, "ok this is a bit longer");
-    menu.changeLocation(150, 150);
+    image = new Image("v2/res/image/cliff.gif");
+    cursor = new Image("v2/res/image/white.png");
+    testmenu.setOptionName("Option 1");
+    testmenu.setOptionName("Option 2");
+    testmenu.setOptionName("Option 3");
+    testmenu.setImage(cursor);
   }
 
   public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics g) throws SlickException {
     g.drawImage(image, 0, 0);
     g.drawString("Now in Test Menu and Music state", 80, 80);
-
-    menu.render(gameContainer, stateBasedGame, g);
+    testmenu.render(g);
   }
 
-  public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int delta) throws SlickException {
-    menu.update(gameContainer, stateBasedGame, delta);
+  public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException {
   }
 
   public void controlPressed(Command command) {
-    menu.controlPressed(command);
+    if (cwInput.isSelectPressed(command)) {
+      menuTickSound.play();
+    }
+    testmenu.controlPressed(command, cwInput);
+  }
+
+  public void controlReleased(Command command) {
   }
 
   public void keyPressed(int key, char c) {
     if (key == Input.KEY_S) {
       backgroundMusic.stop();
     }
-  }
-
-  public void mousePressed(int button, int x, int y) {
-    if (button == Input.MOUSE_RIGHT_BUTTON)
-      menu.changeLocation(x, y);
   }
 
   public int getID() {
