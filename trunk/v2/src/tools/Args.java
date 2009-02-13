2708606e6d6739ed9985a188b4f2d43ef32d6dff
Ace.Stef@gmail.com <Ace.Stef@gmail.com@16bf2f10-a5f7-11dd-a887-7ffe1a420f8d>
2009-02-13 00:49:24 +0000

    Util classes

---
+++ trunk/v2/src/tools/Args.java
@@ -0,0 +1,112 @@
+package tools;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Based on http://www.javapractices.com
+ * All methods throw an <tt>IllegalArgumentException<tt> when the input was not valid
+ *
+ * @author stefan
+ */
+public final class Args {
+
+  /**
+   * This is a static utility class. It cannot be constructed.
+   */
+  private Args() {
+  }
+
+  public static void checkForNull(Object obj) {
+    checkForNull(obj, "Null");
+  }
+
+  public static void checkForContent(String text) {
+    checkForContent(text, "Empty String");
+  }
+
+  public static void checkForNull(Object obj, String errMsg) {
+    if (obj == null) {
+      throw new IllegalArgumentException(errMsg);
+    }
+  }
+
+  public static void checkForContent(String text, String errMsg) {
+    if (text.trim().length() < 0) {
+      throw new IllegalArgumentException(errMsg);
+    }
+  }
+
+  public static void validate(boolean condition, String err) {
+    if (condition) throw new IllegalArgumentException(err);
+  }
+
+  /**
+   * PostCondition:
+   * if amount >= maxamount, return maxAmount
+   * if amount <= 0, return 0
+   */
+  public static int validateBetweenZeroMax(int amount, int maxAmount) {
+    return validateBetweenZeroMax(amount, maxAmount, maxAmount);
+  }
+
+  /**
+   * PostCondition:
+   * if amount >= maxamount, return retAmountWhenMax
+   * if amount <= 0, return 0
+   */
+  public static int validateBetweenZeroMax(int amount, int maxAmount, int retAmountWhenMax) {
+    return validateBetweenMinMax(amount, 0, maxAmount, retAmountWhenMax);
+  }
+
+  /**
+   * Precondition: minAmount < maxAmount
+   * PostCondition:
+   * if amount >= maxamount, return retAmountWhenMax
+   * if amount <= minamount, return 0
+   */
+  public static int validateBetweenMinMax(int amount, int minAmount, int maxAmount, int retAmountWhenMax) {
+    int val = validateBetweenMinMax(amount, minAmount, maxAmount);
+    if (val == maxAmount) {
+      return retAmountWhenMax;
+    } else {
+      return val;
+    }
+  }
+
+  /**
+   * Precondition: minAmount < maxAmount
+   * PostCondition:
+   * if amount >= maxamount, return maxAmount
+   * if amount <= minamount, return minAmount
+   *
+   * @return an amount between minAmount and maxAmount
+   */
+  public static int validateBetweenMinMax(int amount, int minAmount, int maxAmount) {
+    if (minAmount > maxAmount) {
+      throw new IllegalArgumentException("minAmount bigger then maxAmount");
+    }
+
+    int result;
+    if (amount >= maxAmount) {
+      result = maxAmount;
+    } else if (amount < minAmount) {
+      result = minAmount;
+    } else {
+      result = amount;
+    }
+    return result;
+  }
+
+  public static boolean isWithinRange(int amount, int minAmount, int maxAmount) {
+    if (minAmount > maxAmount) {
+      throw new IllegalArgumentException("minAmount bigger then maxAmount");
+    }
+
+    return amount < maxAmount && amount >= minAmount;
+  }
+
+  public static <T> List<T> createEmptyListIfNull(List<T> lst) {
+    return lst == null ? new ArrayList<T>() : lst;
+  }
+}
---
+++ trunk/v2/src/tools/ImageUtil.java
@@ -0,0 +1,21 @@
+package tools;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
+/**
+ * @author stefan
+ */
+public class ImageUtil {
+  /**
+   * Converts an Image to a BufferedImage
+   */
+  public static BufferedImage convertToBufferedImg(Image im) {
+    BufferedImage bi = new BufferedImage(im.getWidth(null), im.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+    Graphics bg = bi.getGraphics();
+    bg.drawImage(im, 0, 0, null);
+    bg.dispose();
+    return bi;
+  }
+}
