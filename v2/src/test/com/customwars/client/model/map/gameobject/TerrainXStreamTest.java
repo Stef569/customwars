353ac86f96918e128d39bfbcdbc0779ed0ea6d3b
Stefan <stef569@users.noreply.github.com>
2009-02-20 21:19:14 +0000

    #36 Adding loading terrain from xml test

---
+++ v2/src/test/com/customwars/client/model/map/gameobject/TerrainXStreamTest.java
@@ -0,0 +1,166 @@
+package test.com.customwars.client.model.map.gameobject;
+
+import com.customwars.client.model.gameobject.Terrain;
+import com.customwars.client.model.gameobject.TerrainFactory;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.converters.ConversionException;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+public class TerrainXStreamTest {
+  private XStream xStream = new XStream(new DomDriver());
+
+  @Before
+  public void beforeEachTest() {
+    // When we find a terrain tag, create a Terrain object
+    // Using Reflection
+    xStream.alias("terrain", Terrain.class);
+    // id and name are read from attributes, not elements
+    xStream.useAttributeFor(Terrain.class, "id");
+    xStream.useAttributeFor(Terrain.class, "name");
+    TerrainFactory.clear();
+  }
+
+  @Test
+  public void testLoadingOfTerrainsFromXml() throws IOException {
+    // HardCoded valid xml string
+    String terrainXML = "<list>" +
+            "  <terrain id='0' name='plain'>" +
+            "    <defenseBonus>0</defenseBonus>" +
+            "    <height>0</height>" +
+            "    <moveCosts>" +
+            "        <int>1</int>" +
+            "        <int>1</int>" +
+            "        <int>1</int>" +
+            "        <int>2</int>" +
+            "        <int>1</int>" +
+            "        <int>127</int>" +
+            "    </moveCosts>" +
+            "    <hidden>false</hidden>" +
+            "  </terrain>" +
+            "<terrain id='1' name='forrest'>" +
+            "    <description></description>" +
+            "    <defenseBonus>0</defenseBonus>" +
+            "    <height>0</height>" +
+            "    <moveCosts>" +
+            "        <int>1</int>" +
+            "        <int>1</int>" +
+            "        <int>1</int>" +
+            "        <int>2</int>" +
+            "        <int>1</int>" +
+            "        <int>127</int>" +
+            "    </moveCosts>" +
+            "    <hidden>false</hidden>" +
+            "  </terrain>" +
+            "</list>";
+
+    // Magic, we retrieve a list of Terrain objects!
+    List<Terrain> terrains = (List<Terrain>) xStream.fromXML(terrainXML);
+    for (Terrain terrain : terrains) {
+      Assert.assertNotNull(terrain);
+      TerrainFactory.addTerrain(terrain);
+    }
+
+    Terrain firstTerrain = TerrainFactory.getTerrain(0);
+    Assert.assertEquals(0, firstTerrain.getID());
+    Assert.assertEquals("plain", firstTerrain.getName());
+
+    // Description is not set in the xml, default to ""!
+    Assert.assertEquals("", firstTerrain.getDescription());
+    Assert.assertEquals(TerrainFactory.countTerrains(), 2);
+  }
+
+  /**
+   * IllegalArgumentException: 1800 is not valid
+   */
+  @Test(expected = IllegalArgumentException.class)
+  public void testInvalidTerrainXMLInvalidMovecostInput_1800() {
+    // HardCoded invalid xml string
+    // <int>1800</int> is not within min and max movecost
+    String invalidTerrainXML =
+            "  <terrain id='0' name='plain'>" +
+                    "    <moveCosts>" +
+                    "        <int>1800</int>" +
+                    "    </moveCosts>" +
+                    "  </terrain>";
+
+    Terrain terrain = (Terrain) xStream.fromXML(invalidTerrainXML);
+    TerrainFactory.addTerrain(terrain);
+    TerrainFactory.getRandomTerrain();
+  }
+
+  /**
+   * IllegalArgumentException: 0 is not valid
+   */
+  @Test(expected = IllegalArgumentException.class)
+  public void testInvalidTerrainXMLInvalidMovecostInput_0() {
+    // HardCoded invalid xml string
+    // <int>0</int> is not within min and max movecost
+    String invalidTerrainXML =
+            "  <terrain id='0' name='plain'>" +
+                    "    <moveCosts>" +
+                    "        <int>0</int>" +
+                    "    </moveCosts>" +
+                    "  </terrain>";
+
+    Terrain terrain = (Terrain) xStream.fromXML(invalidTerrainXML);
+    TerrainFactory.addTerrain(terrain);
+    TerrainFactory.getRandomTerrain();
+  }
+
+  /**
+   * IllegalArgumentException: addTerrain(terrain) will not add the duplicate ID
+   */
+  @Test(expected = IllegalArgumentException.class)
+  public void testInvalidTerrainXML() {
+    // Contains duplicate ID
+    String invalidTerrainXML = "<list>" +
+            "  <terrain id='1' name='plain1'>" +
+            "    <moveCosts/>" +
+            "  </terrain>" +
+            "  <terrain id='1' name='plain2'>" +
+            "    <moveCosts/>" +
+            "  </terrain>" +
+            "</list>";
+
+    List<Terrain> terrainsFromXml = (List<Terrain>) xStream.fromXML(invalidTerrainXML);
+    for (Terrain terrain : terrainsFromXml) {
+      TerrainFactory.addTerrain(terrain);
+    }
+  }
+
+  /**
+   * ConversionException: DefenseBonus should be defenseBonus
+   */
+  @Test(expected = ConversionException.class)
+  public void testIllegalFieldCase() {
+    // <DefenseBonus>0</DefenseBonus> upper case 'D' is invalid
+    // should match the field in the class
+    String invalidTerrainXML = "<list>" +
+            "  <terrain id='0' name='plain'>" +
+            "    <DefenseBonus>0</DefenseBonus>" +
+            "    <height>0</height>" +
+            "    <moveCosts>" +
+            "        <int>5</int>" +
+            "    </moveCosts>" +
+            "  </terrain>" +
+            "  <terrain id='1' name='plain'>" +
+            "    <defenseBonus>0</defenseBonus>" +
+            "    <height>0</height>" +
+            "    <moveCosts>" +
+            "        <int>5</int>" +
+            "    </moveCosts>" +
+            "  </terrain>" +
+            "</list>";
+
+    List<Terrain> terrainsFromXml = (List<Terrain>) xStream.fromXML(invalidTerrainXML);
+    for (Terrain terrain : terrainsFromXml) {
+      TerrainFactory.addTerrain(terrain);
+    }
+  }
+}
