e409cbcf3bf93039d057d7cde2069d95cc611b03
Stefan <stef569@users.noreply.github.com>
2009-03-21 13:05:28 +0000

    Forgot to add unversioned file...

---
+++ v2/src/com/customwars/client/action/unit/AttackAction.java
@@ -0,0 +1,42 @@
+package com.customwars.client.action.unit;
+
+import com.customwars.client.action.DirectAction;
+import com.customwars.client.model.gameobject.Unit;
+import com.customwars.client.model.map.UnitFight;
+import com.customwars.client.ui.state.InGameContext;
+
+/**
+ * Attack defender with attacker
+ *
+ * @author stefan
+ */
+public class AttackAction extends DirectAction {
+  private InGameContext context;
+  private UnitFight unitFight;
+  private Unit attacker, defender;
+
+  public AttackAction(Unit attacker, Unit defender) {
+    super("Attack", false);
+    this.attacker = attacker;
+    this.defender = defender;
+  }
+
+  protected void init(InGameContext context) {
+    this.context = context;
+    unitFight = new UnitFight(context.getGame().getMap());
+  }
+
+  protected void invokeAction() {
+    if (context.isTrapped()) return;
+    attackUnit(attacker, defender);
+  }
+
+  /**
+   * @param attacker The unit that is attacking
+   * @param defender The Unit that is under attacked
+   */
+  public void attackUnit(Unit attacker, Unit defender) {
+    unitFight.initAttack(attacker, defender);
+    attacker.attack(defender, unitFight);
+  }
+}
