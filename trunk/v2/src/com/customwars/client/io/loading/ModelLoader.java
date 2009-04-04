5d86075c8e3f0531c95935a2c934afa620b1c383
Ace.Stef <Ace.Stef@16bf2f10-a5f7-11dd-a887-7ffe1a420f8d>
2009-04-04 14:26:33 +0000

    Read damage tabels, add them to UnitFight XstreamUtil for writing/reading xml in 1 line(handles exceptions, closing in/out put streams... Removing unused method clear() in ResourceManager

--- trunk/v2/src/com/customwars/client/io/loading/ModelLoader.java
+++ trunk/v2/src/com/customwars/client/io/loading/ModelLoader.java
@@ -7,6 +7,7 @@ import com.customwars.client.model.gameobject.Terrain;
 import com.customwars.client.model.gameobject.TerrainFactory;
 import com.customwars.client.model.gameobject.Unit;
 import com.customwars.client.model.gameobject.UnitFactory;
+import com.customwars.client.model.gameobject.UnitFight;
 import com.customwars.client.model.gameobject.Weapon;
 import com.customwars.client.model.gameobject.WeaponFactory;
 import com.thoughtworks.xstream.XStream;
@@ -14,13 +15,15 @@ import com.thoughtworks.xstream.io.xml.DomDriver;
 import org.newdawn.slick.loading.DeferredResource;
 import org.newdawn.slick.loading.LoadingList;
 import org.newdawn.slick.util.ResourceLoader;
+import tools.XStreamUtil;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collection;
 
 /**
- * Loads Model classes to their Factories ie
+ * Loads: Damage tables,
+ * Model classes to their Factories ie
  * Terrain -> TerrainFactory
  * Weapon -> WeaponFactory
  * Unit -> UnitFactory
@@ -29,11 +32,14 @@ import java.util.Collection;
  * @author stefan
  */
 public class ModelLoader {
-  private static final XStream xStream = new XStream(new DomDriver());
   private static final String XML_DATA_TERRAIN_FILE = "terrains.xml";
   private static final String XML_DATA_WEAPONS_FILE = "weapons.xml";
   private static final String XML_DATA_UNITS_FILE = "units.xml";
   private static final String XML_DATA_CITY_FILE = "cities.xml";
+  private static final String BASE_DMG_FILE = "baseDMG.txt";
+  private static final String ALT_DMG_FILE = "altDMG.txt";
+  private static final XStream xStream = new XStream(new DomDriver());
+  private static final DamageParser damageParser = new DamageParser();
   private String modelResPath;
 
   public void loadModel() {
@@ -49,6 +55,7 @@ public class ModelLoader {
     loadWeapons();
     loadUnits();
     loadCities();
+    loadDamageTables();
   }
 
   private void loadTerrains() {
@@ -56,7 +63,8 @@ public class ModelLoader {
     xStream.useAttributeFor(Terrain.class, "id");
     xStream.useAttributeFor(Terrain.class, "name");
     InputStream terrainStream = ResourceLoader.getResourceAsStream(modelResPath + XML_DATA_TERRAIN_FILE);
-    TerrainFactory.addTerrains((Collection<Terrain>) xStream.fromXML(terrainStream));
+    Collection<Terrain> terrains = (Collection<Terrain>) XStreamUtil.readObject(xStream, terrainStream);
+    TerrainFactory.addTerrains(terrains);
   }
 
   private void loadWeapons() {
@@ -64,7 +72,8 @@ public class ModelLoader {
     xStream.useAttributeFor(Weapon.class, "id");
     xStream.useAttributeFor(Weapon.class, "name");
     InputStream weaponStream = ResourceLoader.getResourceAsStream(modelResPath + XML_DATA_WEAPONS_FILE);
-    WeaponFactory.addWeapons((Collection<Weapon>) xStream.fromXML(weaponStream));
+    Collection<Weapon> weapons = (Collection<Weapon>) XStreamUtil.readObject(xStream, weaponStream);
+    WeaponFactory.addWeapons(weapons);
   }
 
   private void loadUnits() {
@@ -75,7 +84,8 @@ public class ModelLoader {
     xStream.useAttributeFor(Unit.class, "id");
     xStream.useAttributeFor(Unit.class, "name");
     InputStream unitStream = ResourceLoader.getResourceAsStream(modelResPath + XML_DATA_UNITS_FILE);
-    UnitFactory.addUnits((Collection<Unit>) xStream.fromXML(unitStream));
+    Collection<Unit> units = (Collection<Unit>) XStreamUtil.readObject(xStream, unitStream);
+    UnitFactory.addUnits(units);
   }
 
   private void loadCities() {
@@ -83,14 +93,20 @@ public class ModelLoader {
     xStream.useAttributeFor(Terrain.class, "id");
     xStream.useAttributeFor(Terrain.class, "name");
     InputStream cityStream = ResourceLoader.getResourceAsStream(modelResPath + XML_DATA_CITY_FILE);
-    CityFactory.addCities((Collection<City>) xStream.fromXML(cityStream));
+    Collection<City> cities = (Collection<City>) XStreamUtil.readObject(xStream, cityStream);
+    CityFactory.addCities(cities);
   }
 
-  public void clear() {
-    TerrainFactory.clear();
-    WeaponFactory.clear();
-    UnitFactory.clear();
-    CityFactory.clear();
+  private void loadDamageTables() {
+    InputStream baseDamageStream = ResourceLoader.getResourceAsStream(modelResPath + BASE_DMG_FILE);
+    InputStream altDamageStream = ResourceLoader.getResourceAsStream(modelResPath + ALT_DMG_FILE);
+
+    try {
+      UnitFight.setBaseDMG(damageParser.read(baseDamageStream));
+      UnitFight.setAltDMG(damageParser.read(altDamageStream));
+    } catch (IOException e) {
+      throw new RuntimeException("Could not read damage table", e);
+    }
   }
 
   public void setModelResPath(String modelResPath) {
