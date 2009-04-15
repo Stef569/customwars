5e9f4d08aa45b522bad51c7aebc405e76233754d
Ace.Stef <Ace.Stef@16bf2f10-a5f7-11dd-a887-7ffe1a420f8d>
2009-04-15 00:58:36 +0000

    Refactored Percentage calculation into NumberUtil

---
+++ trunk/v2/src/tools/NumberUtil.java
@@ -0,0 +1,29 @@
+package tools;
+
+public final class NumberUtil {
+  /**
+   * This is a static utility class. It cannot be constructed.
+   */
+  private NumberUtil() {
+  }
+
+  public static int findHighest(int... values) {
+    int highest = 0;
+
+    for (int val : values) {
+      if (val > highest) highest = val;
+    }
+    return highest;
+  }
+
+  public static int calcPercentage(int val, int max) {
+    int percentage;
+    if (max <= 0) {
+      percentage = 0;
+    } else {
+      double divide = (double) val / max;
+      percentage = (int) Math.round(divide * 100);
+    }
+    return percentage;
+  }
+}
