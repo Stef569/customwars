5cee2d44525d0e8483986c935c6f043844ff3e6f
Stefan <stef569@users.noreply.github.com>
2009-04-15 00:58:36 +0000

    Refactored Percentage calculation into NumberUtil

--- v2/src/com/customwars/client/model/gameobject/Weapon.java
+++ v2/src/com/customwars/client/model/gameobject/Weapon.java
@@ -1,6 +1,7 @@
 package com.customwars.client.model.gameobject;
 
 import tools.Args;
+import tools.NumberUtil;
 
 /**
  * A weapon has an amount of ammo, it can fire within a fire range
@@ -12,18 +13,16 @@ public class Weapon extends GameObject {
   private int id;
   private String name;
   private String description;
-  private int price;
   private int minFireRange;
   private int maxFireRange;
   private int maxAmmo;
   private boolean balistic;   // Gives this weapon indirect firing ability even after moving
   private int ammo;
 
-  public Weapon(int id, String name, String description, int price, int minRange, int maxRange, int maxAmmo, boolean balistic) {
+  public Weapon(int id, String name, String description, int minRange, int maxRange, int maxAmmo, boolean balistic) {
     this.id = id;
     this.name = name;
     this.description = description;
-    this.price = price;
     this.minFireRange = minRange;
     this.maxFireRange = maxRange;
     this.maxAmmo = maxAmmo;
@@ -47,7 +46,6 @@ public class Weapon extends GameObject {
     id = otherWeapon.id;
     name = otherWeapon.name;
     description = otherWeapon.description;
-    price = otherWeapon.price;
     minFireRange = otherWeapon.minFireRange;
     maxFireRange = otherWeapon.maxFireRange;
     maxAmmo = otherWeapon.maxAmmo;
@@ -89,10 +87,6 @@ public class Weapon extends GameObject {
     return description;
   }
 
-  public int getPrice() {
-    return price;
-  }
-
   public int getMinRange() {
     return minFireRange;
   }
@@ -109,33 +103,19 @@ public class Weapon extends GameObject {
     return ammo;
   }
 
+  /**
+   * @return true if this weapon has some ammo left
+   */
+  public boolean canFire() {
+    return ammo > 0;
+  }
+
   public boolean isWithinRange(int range) {
     return range >= minFireRange && range <= maxFireRange;
   }
 
-  /**
-   * @return true if this weapon has no ammo left
-   */
-  public boolean isDepleted() {
-    return (ammo <= 0);
-  }
-
-  /**
-   * @return the price it would cost to restock the ammo for this weapon.
-   */
-  public int getRestockPrice() {
-    return ((maxAmmo - ammo) * price);
-  }
-
   public int getAmmoPercentage() {
-    int percentage;
-    if (maxAmmo <= 0) {
-      percentage = 100;
-    } else {
-      double divide = (double) ammo / maxAmmo;
-      percentage = (int) Math.round(divide * 100);
-    }
-    return percentage;
+    return NumberUtil.calcPercentage(ammo, maxAmmo);
   }
 
   public boolean isBalistic() {
@@ -144,6 +124,6 @@ public class Weapon extends GameObject {
 
   @Override
   public String toString() {
-    return "[name=" + name + " id=" + id + " ammo=" + ammo + "]";
+    return String.format("[name=%s id=%s ammo=%s/%s range=%s/%s]", name, id, ammo, maxAmmo, minFireRange, maxFireRange);
   }
 }
