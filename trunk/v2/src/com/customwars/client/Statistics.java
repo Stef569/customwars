b4817afb7371dae7e4da5a3002dee04d812da106
Ace.Stef <Ace.Stef@16bf2f10-a5f7-11dd-a887-7ffe1a420f8d>
2009-04-24 21:02:12 +0000

    Stats about the game, including: units killed/lost, cities captured. Units killing themselfs is a problem since there is no way to find out what player the unit was attacking so they are not included in the stats.

---
+++ trunk/v2/src/com/customwars/client/Statistics.java
@@ -0,0 +1,93 @@
+package com.customwars.client;
+
+import com.customwars.client.model.game.Game;
+import com.customwars.client.model.game.Player;
+import com.customwars.client.model.gameobject.City;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Gather statistics about the given game
+ *
+ * @author stefan
+ */
+public class Statistics implements PropertyChangeListener {
+  private Game game;
+  private Map<Player, PlayerStatistics> playerStatistics;
+
+  public Statistics(Game game) {
+    this.game = game;
+    addListeners(game);
+    initPlayerStatistics();
+  }
+
+  private void addListeners(Game game) {
+    for (Player p : game.getAllPlayers()) {
+      p.addPropertyChangeListener(this);
+
+      for (City city : p.getAllCities()) {
+        city.addPropertyChangeListener(this);
+      }
+    }
+
+    Player neutralPlayer = game.getPlayerByID(Player.NEUTRAL_PLAYER_ID);
+    for (City neutralCity : neutralPlayer.getAllCities()) {
+      neutralCity.addPropertyChangeListener(this);
+    }
+  }
+
+  private void initPlayerStatistics() {
+    playerStatistics = new HashMap<Player, PlayerStatistics>();
+    for (Player player : game.getAllPlayers()) {
+      playerStatistics.put(player, new PlayerStatistics());
+    }
+  }
+
+  public PlayerStatistics getPlayerStats(Player player) {
+    return playerStatistics.get(player);
+  }
+
+  public void propertyChange(PropertyChangeEvent evt) {
+    // Only gather statistics when the game has started
+    if (!game.isActive()) return;
+    String propertyName = evt.getPropertyName();
+
+    if (evt.getSource() instanceof Player) {
+      if (propertyName.equals("unit")) {
+        Player player = (Player) evt.getSource();
+        Player activePlayer = game.getActivePlayer();
+        PlayerStatistics playerStats = playerStatistics.get(player);
+
+        if (evt.getOldValue() == null && evt.getNewValue() != null) {
+          playerStats.unitsCreated++;
+        } else if (evt.getOldValue() != null && evt.getNewValue() == null) {
+          playerStats.unitsLost++;
+
+          // Don't record units killing themselfs..
+          if (activePlayer != player) {
+            PlayerStatistics activePlayerStats = getActivePlayerStats();
+            activePlayerStats.unitsKilled++;
+          }
+        }
+      }
+    } else if (evt.getSource() instanceof City) {
+      if (propertyName.equals("captured")) {
+        if ((Boolean) evt.getNewValue()) {
+          PlayerStatistics activePlayerStats = getActivePlayerStats();
+          activePlayerStats.citiesCaptured++;
+        }
+      }
+    }
+  }
+
+  private PlayerStatistics getActivePlayerStats() {
+    return playerStatistics.get(game.getActivePlayer());
+  }
+
+  public class PlayerStatistics {
+    public int unitsCreated, unitsKilled, unitsLost, citiesCaptured;
+  }
+}
--- trunk/v2/src/com/customwars/client/ui/state/GameOverState.java
+++ trunk/v2/src/com/customwars/client/ui/state/GameOverState.java
@@ -1,13 +1,20 @@
 package com.customwars.client.ui.state;
 
+import com.customwars.client.Statistics;
+import com.customwars.client.model.game.Player;
+import com.customwars.client.ui.layout.TextBox;
 import org.apache.log4j.Logger;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameOverState extends CWState {
   private Logger logger = Logger.getLogger(GameOverState.class);
+  List<TextBox> table = new ArrayList<TextBox>();
 
   public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
   }
@@ -15,11 +22,43 @@ public class GameOverState extends CWState {
   @Override
   public void enter(GameContainer container, StateBasedGame game) throws SlickException {
     super.enter(container, game);
+    Statistics stats = stateSession.stats;
+
+    // todo create a Table gui object where rows and columns can be added, instead of this mess.
+    // todo create column for each player, create row for each statistic
+    table.clear();
+    int row = 0;
+    for (Player p : stateSession.game.getAllPlayers()) {
+      Statistics.PlayerStatistics playerStats = stats.getPlayerStats(p);
+
+      TextBox header = new TextBox("Player " + p.getName(), defaultFont);
+      TextBox unitsCreated = new TextBox("Units created: " + playerStats.unitsCreated, defaultFont);
+      TextBox unitsKilled = new TextBox("Units killed: " + playerStats.unitsKilled, defaultFont);
+      TextBox unitsLost = new TextBox("Units lost: " + playerStats.unitsLost, defaultFont);
+      TextBox citiesCapped = new TextBox("cities capped: " + playerStats.citiesCaptured, defaultFont);
+
+      header.setLocation(50 + row * 150, 80);
+      table.add(header);
+      unitsCreated.setLocation(50 + row * 150, 90);
+      table.add(unitsCreated);
+      unitsKilled.setLocation(50 + row * 150, 100);
+      table.add(unitsKilled);
+      unitsLost.setLocation(50 + row * 150, 110);
+      table.add(unitsLost);
+      citiesCapped.setLocation(50 + row * 150, 120);
+      table.add(citiesCapped);
+      row++;
+    }
+
     logger.info("Game Over");
   }
 
   public void render(GameContainer container, Graphics g) throws SlickException {
-    g.drawString("GAME OVER ", 100, 150);
+    g.drawString("GAME OVER, Statistics: ", 100, 40);
+
+    for (TextBox txtBox : table) {
+      txtBox.render(g);
+    }
   }
 
   public void update(GameContainer container, int delta) throws SlickException {
