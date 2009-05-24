53bafeb5f3b029591ea1f6856e39807b299d403a
Stefan <stef569@users.noreply.github.com>
2009-05-24 11:38:26 +0000

    Improving end turn state, previous version showed next player after ending the turn.

--- v2/test/slick/EndTurnState.java
+++ v2/test/slick/EndTurnState.java
@@ -3,7 +3,6 @@ package slick;
 import com.customwars.client.model.game.Game;
 import com.customwars.client.model.game.Player;
 import com.customwars.client.ui.state.CWState;
-import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
@@ -11,23 +10,31 @@ import org.newdawn.slick.state.StateBasedGame;
 
 /**
  * Show information about the next turn
- * and end the current turn when the delay has passed
+ * end the current turn when the delay has passed
  */
 public class EndTurnState extends CWState {
   private static final int END_TURN_DELAY = 250;
   private int timeTaken;
   private Game game;
+  private Player nextPlayer;
+  private int nextDay;
 
   public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
   }
 
-  public void render(GameContainer container, Graphics g) throws SlickException {
+  @Override
+  public void enter(GameContainer container, StateBasedGame stateBasedGame) throws SlickException {
+    super.enter(container, stateBasedGame);
     game = stateSession.game;
-    g.setColor(Color.white);
-
-    Player nextActivePlayer = game.getNextActivePlayer(game.getActivePlayer());
+    nextPlayer = game.getNextActivePlayer(game.getActivePlayer());
     int turnCount = game.getTurn() + 1;
-    g.drawString("Day " + game.getDay(turnCount) + " " + nextActivePlayer.getName() + " Make your moves", 150, 150);
+    nextDay = game.getDay(turnCount);
+  }
+
+  public void render(GameContainer container, Graphics g) throws SlickException {
+    if (entered) {
+      g.drawString("Day " + nextDay + " " + nextPlayer.getName() + " Make your moves", 150, 150);
+    }
   }
 
   public void update(GameContainer container, int delta) throws SlickException {
