9fcc0e5da7080fb02d9d3619af4761a8cd8fbcef
Stefan <stef569@users.noreply.github.com>
2010-01-10 19:05:25 +0000

    Hide how city/unit animations are retrieved from the AnimLib.

---
+++ v2/src/com/customwars/client/io/img/CWAnimLib.java
@@ -0,0 +1,53 @@
+package com.customwars.client.io.img;
+
+import com.customwars.client.model.gameobject.City;
+import com.customwars.client.model.gameobject.Unit;
+import com.customwars.client.model.map.Direction;
+import org.newdawn.slick.Animation;
+
+import java.awt.Color;
+
+/**
+ * CW specific Animation getter functions
+ * This class hides how unit/city Animations are retrieved from the AnimLib
+ */
+public class CWAnimLib {
+  private final AnimLib animLib;
+
+  public CWAnimLib(AnimLib animLib) {
+    this.animLib = animLib;
+  }
+
+  public Animation getCityAnim(City city, Color color) {
+    return animLib.getCityAnim(city.getID(), color, "");
+  }
+
+  public Animation getFoggedCityAnim(City city, Color color) {
+    return animLib.getCityAnim(city.getID(), color, AnimLib.ANIM_FOGGED);
+  }
+
+  /**
+   * Get a single unit animation that is looking in the given direction.
+   * Supported directions(N,E,S,W) all other directions will throw an IllegalArgumentException
+   */
+  public Animation getUnitAnim(Unit unit, Color color, Direction direction) {
+    int unitID = unit.getStats().getID();
+
+    switch (direction) {
+      case NORTH:
+        return animLib.getUnitAnim(unitID, color, AnimLib.ANIM_UP);
+      case EAST:
+        return animLib.getUnitAnim(unitID, color, AnimLib.ANIM_RIGHT);
+      case SOUTH:
+        return animLib.getUnitAnim(unitID, color, AnimLib.ANIM_DOWN);
+      case WEST:
+        return animLib.getUnitAnim(unitID, color, AnimLib.ANIM_LEFT);
+      default:
+        throw new IllegalArgumentException("Direction " + direction + " is not supported for a unit animation");
+    }
+  }
+
+  public Animation getInactiveUnitAnim(Unit unit, Color color) {
+    return animLib.getUnitAnim(unit.getStats().getImgRowID(), color, AnimLib.ANIM_INACTIVE);
+  }
+}
