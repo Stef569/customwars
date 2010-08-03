b1bacf1d078c2b4457b47ad8d4cefe5ad78b6cce
Stefan <stef569@users.noreply.github.com>
2010-08-03 17:12:04 +0000

    Don't show arrows when selecting the flare location.

--- v2/src/com/customwars/client/action/unit/StartFlareAction.java
+++ v2/src/com/customwars/client/action/unit/StartFlareAction.java
@@ -7,6 +7,10 @@ import com.customwars.client.model.gameobject.Unit;
 import com.customwars.client.ui.renderer.MapRenderer;
 import com.customwars.client.ui.state.InGameContext;
 
+/**
+ * The Start Flare action allows the user to choose a flare location.
+ * The flare range is shown as an attack zone.
+ */
 public class StartFlareAction extends DirectAction {
   private InGameContext inGameContext;
   private MapRenderer mapRenderer;
@@ -30,6 +34,7 @@ public class StartFlareAction extends DirectAction {
     Unit activeUnit = game.getActiveUnit();
     game.getMap().buildFlareZone(activeUnit);
     mapRenderer.showAttackZone();
+    mapRenderer.showArrows(false);
     cursorController.activateCursor("SILO");
     cursorController.moveCursor(activeUnit.getAttackZone().get(0));
     inGameContext.setInputMode(InGameContext.INPUT_MODE.UNIT_FLARE);
@@ -38,6 +43,7 @@ public class StartFlareAction extends DirectAction {
   @Override
   public void undo() {
     mapRenderer.removeZones();
+    mapRenderer.showArrows(true);
     cursorController.activateCursor("SELECT");
     inGameContext.setInputMode(InGameContext.INPUT_MODE.DEFAULT);
   }
