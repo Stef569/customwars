0b0c116ba7e5a82e0d1fe708aa4665893ebd4571
Stefan <stef569@users.noreply.github.com>
2015-01-11 11:57:04 +0000

    Issue 120: The move path (arrows) should not be visible when selecting the rocket destination. This caused the unit to move on the path instead of moving towards the city causing a visual glitch. Glitches are bad.

--- v2/src/com/customwars/client/action/city/StartLaunchRocketAction.java
+++ v2/src/com/customwars/client/action/city/StartLaunchRocketAction.java
@@ -2,16 +2,16 @@ package com.customwars.client.action.city;
 
 import com.customwars.client.action.DirectAction;
 import com.customwars.client.controller.CursorController;
+import com.customwars.client.ui.renderer.MapRenderer;
 import com.customwars.client.ui.state.InGameContext;
 
 /**
  * Shows a Silo cursor
- *
- * @author stefan
  */
 public class StartLaunchRocketAction extends DirectAction {
   private InGameContext inGameContext;
   private CursorController cursorControl;
+  private MapRenderer mapRenderer;
 
   public StartLaunchRocketAction() {
     super("Start launch rocket", true);
@@ -21,17 +21,22 @@ public class StartLaunchRocketAction extends DirectAction {
   protected void init(InGameContext inGameContext) {
     this.inGameContext = inGameContext;
     cursorControl = inGameContext.getObj(CursorController.class);
+    mapRenderer = inGameContext.getObj(MapRenderer.class);
   }
 
   @Override
   protected void invokeAction() {
     inGameContext.setInputMode(InGameContext.INPUT_MODE.LAUNCH_ROCKET);
     cursorControl.activateCursor("SILO");
+
+    // Don't show arrows when choosing a rocket destination
+    mapRenderer.showArrows(false);
   }
 
   @Override
   public void undo() {
     inGameContext.setInputMode(InGameContext.INPUT_MODE.DEFAULT);
     cursorControl.activateCursor("SELECT");
+    mapRenderer.showArrows(true);
   }
 }
