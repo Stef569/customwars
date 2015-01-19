2e741067cd3fbbe7b25fa33630e4ab2135ced420
ace.Stef@gmail.com <ace.Stef@gmail.com@16bf2f10-a5f7-11dd-a887-7ffe1a420f8d>
2015-01-19 16:58:03 +0000

    Fix issue 139: Minimap Zoom Unlimited: There is now a limits to zooming, 2 buttons (+)(-) are included in the map select state. Moved methods from MapSelectState to controller where they belong. Added + and : to armalite.ttf font as they were missing.

--- trunk/v2/src/com/customwars/client/controller/MapSelectController.java
+++ trunk/v2/src/com/customwars/client/controller/MapSelectController.java
@@ -3,6 +3,7 @@ package com.customwars.client.controller;
 import com.customwars.client.io.ResourceManager;
 import com.customwars.client.model.map.Map;
 import com.customwars.client.tools.StringUtil;
+import com.customwars.client.tools.ThingleUtil;
 import com.customwars.client.ui.renderer.widget.CityCountWidgetRenderer;
 import com.customwars.client.ui.renderer.widget.MiniMapWidgetRenderer;
 import com.customwars.client.ui.state.StateChanger;
@@ -10,16 +11,26 @@ import com.customwars.client.ui.state.StateSession;
 import org.newdawn.slick.thingle.Page;
 import org.newdawn.slick.thingle.Widget;
 
+import java.util.List;
+
 /**
  * Handles input in the MapSelectState
  */
 public class MapSelectController {
-  private Page page;
+  private static final float MINIMAP_SCALE_STEP = 0.5f;
+  private static final float MINIMAP_INITIAL_SCALE = 3f;
+  private static final float MINIMAP_MAX_SCALE = 7f;
+  private static final float MINIMAP_MIN_SCALE = 0.5f;
+
   private final ResourceManager resources;
   private final MiniMapWidgetRenderer miniMapRenderer;
   private final CityCountWidgetRenderer citiesRenderer;
   private final StateChanger stateChanger;
   private final StateSession stateSession;
+
+  private float miniMapScale;
+  private boolean miniMapScaleLoopDirectionUP;
+  private Page page;
 
   public MapSelectController(ResourceManager resources, MiniMapWidgetRenderer miniMapRenderer, CityCountWidgetRenderer citiesRenderer, StateChanger stateChanger, StateSession stateSession) {
     this.resources = resources;
@@ -33,19 +44,47 @@ public class MapSelectController {
     this.page = page;
   }
 
-  public void mapSelected() {
-    Map map = getCurrentSelectedMap();
+  public void enter() {
+    miniMapScale = MINIMAP_INITIAL_SCALE;
+    loadMapCategories();
+  }
 
-    Widget mapNameWidget = page.getWidget("map_name");
-    if (StringUtil.hasContent(map.getAuthor())) {
-      mapNameWidget.setText(map.getMapName() + " by " + map.getAuthor());
-    } else {
-      mapNameWidget.setText(map.getMapName());
+  public void loadMapCategories() {
+    List<String> mapCategories = resources.getAllMapCategories();
+    initPageContent(mapCategories);
+    initFilter(mapCategories);
+  }
+
+  private void initPageContent(List<String> mapCategories) {
+    if (!mapCategories.isEmpty()) {
+      Widget mapCategoryCbo = page.getWidget("map_categories");
+      mapCategoryCbo.removeChildren();
+
+      for (String mapCategory : mapCategories) {
+        ThingleUtil.addChoice(page, mapCategoryCbo, mapCategory);
+      }
+
+      selectDefaultMapCategory(mapCategoryCbo);
     }
-    page.getWidget("map_description").setText(map.getDescription());
+  }
 
-    miniMapRenderer.setMap(map);
-    citiesRenderer.setMap(map);
+  private void selectDefaultMapCategory(Widget mapCategoryCbo) {
+    // The previous category value is remembered
+    // Only select the default first category if none is chosen
+    int selectedIndex = mapCategoryCbo.getSelectedIndex();
+
+    if (selectedIndex == -1) {
+      mapCategoryCbo.setInteger("selected", 0);
+    }
+  }
+
+  private void initFilter(List<String> mapCategories) {
+    if (!mapCategories.isEmpty()) {
+      Widget mapCategoryCbo = page.getWidget("map_categories");
+      int selectedIndex = mapCategoryCbo.getSelectedIndex();
+      String firstMapCategory = mapCategories.get(selectedIndex);
+      filterMapsOnCategory(firstMapCategory);
+    }
   }
 
   public void filterMapsOnCategory(String mapCategory) {
@@ -65,6 +104,23 @@ public class MapSelectController {
     }
   }
 
+  public void mapSelected() {
+    Map map = getCurrentSelectedMap();
+    String mapName = map.getMapName();
+    String mapAuthor = map.getAuthor();
+
+    Widget mapNameWidget = page.getWidget("map_name");
+    if (StringUtil.hasContent(map.getAuthor())) {
+      mapNameWidget.setText(mapName + " by " + mapAuthor);
+    } else {
+      mapNameWidget.setText(mapName);
+    }
+    page.getWidget("map_description").setText(map.getDescription());
+
+    miniMapRenderer.setMap(map);
+    citiesRenderer.setMap(map);
+  }
+
   private Map getCurrentSelectedMap() {
     Widget mapList = page.getWidget("map_list");
     Widget selectedItem = mapList.getSelectedWidget();
@@ -74,6 +130,41 @@ public class MapSelectController {
   public void continueToNextState() {
     stateSession.map = getCurrentSelectedMap();
     stateChanger.changeTo("PLAYER_OPTIONS");
+  }
+
+  public void addPressed() {
+    if (miniMapScaleLoopDirectionUP) {
+      miniMapScale = getValidScale(miniMapScale + MINIMAP_SCALE_STEP);
+    } else {
+      miniMapScale = getValidScale(miniMapScale - MINIMAP_SCALE_STEP);
+    }
+    miniMapRenderer.setScale(miniMapScale);
+
+    if (miniMapScale == MINIMAP_MAX_SCALE) {
+      miniMapScaleLoopDirectionUP = false;
+    } else if (miniMapScale == MINIMAP_MIN_SCALE) {
+      miniMapScaleLoopDirectionUP = true;
+    }
+  }
+
+  public void zoomIn() {
+    miniMapScale = getValidScale(miniMapScale + MINIMAP_SCALE_STEP);
+    miniMapRenderer.setScale(miniMapScale);
+  }
+
+  public void zoomOut() {
+    miniMapScale = getValidScale(miniMapScale - MINIMAP_SCALE_STEP);
+    miniMapRenderer.setScale(miniMapScale);
+  }
+
+  private float getValidScale(float newScale) {
+    if (newScale > MINIMAP_MAX_SCALE) {
+      return MINIMAP_MAX_SCALE;
+    } else if (newScale < MINIMAP_MIN_SCALE) {
+      return MINIMAP_MIN_SCALE;
+    } else {
+      return newScale;
+    }
   }
 
   public void back() {
