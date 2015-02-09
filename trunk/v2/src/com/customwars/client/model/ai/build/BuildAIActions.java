37e6fb03205e1d071f65ee58ae95a10b5cb6c347
ace.Stef@gmail.com <ace.Stef@gmail.com@16bf2f10-a5f7-11dd-a887-7ffe1a420f8d>
2015-02-09 14:27:24 +0000

    Use the active player from the Game and not the city owner in the AI game copy.

--- trunk/v2/src/com/customwars/client/model/ai/build/BuildAIActions.java
+++ trunk/v2/src/com/customwars/client/model/ai/build/BuildAIActions.java
@@ -2,6 +2,7 @@ package com.customwars.client.model.ai.build;
 
 import com.customwars.client.action.ActionFactory;
 import com.customwars.client.action.CWAction;
+import com.customwars.client.model.game.Game;
 import com.customwars.client.model.game.Player;
 import com.customwars.client.model.gameobject.City;
 import com.customwars.client.model.gameobject.Unit;
@@ -14,10 +15,12 @@ import java.util.Map;
  * Creates the Build Actions for each Factory
  */
 public class BuildAIActions {
+  private final Player activePlayer;
   private final Map<City, Unit> unitsToBuild;
   private final List<CWAction> actions;
 
-  public BuildAIActions(Map<City, Unit> unitsToBuild) {
+  public BuildAIActions(Map<City, Unit> unitsToBuild, Game game) {
+    this.activePlayer = game.getActivePlayer();
     this.unitsToBuild = unitsToBuild;
     actions = new ArrayList<CWAction>();
   }
@@ -35,8 +38,7 @@ public class BuildAIActions {
    * Builds the unit and add it to the map
    */
   private void buildUnit(City city, Unit unit) {
-    Player p = city.getOwner();
-    CWAction buildUnitAction = ActionFactory.buildAddUnitToTileAction(unit, p, city.getLocation());
+    CWAction buildUnitAction = ActionFactory.buildAddUnitToTileAction(unit, activePlayer, city.getLocation());
     actions.add(buildUnitAction);
   }
 }
